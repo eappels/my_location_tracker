46cabcb40a6556ac80f45cde7e13dc64e31322dd
appels <appels@telenet.be>
2025-05-25 16:34:17 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# my_location_tracker
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/my_location_tracker/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.my_location_tracker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
